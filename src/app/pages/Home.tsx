9e24cea777c68e888ce57cca580298fecd72b5ee
Goenka <nikhil.goenka@dt-dx165t2h50.clients.dynatrace.org>
2024-10-30 00:44:54 +1100

    Push initial changes

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -13,49 +13,43 @@ export const Home = () => {
   const theme = useCurrentTheme();
   return (
     <Flex flexDirection="column" alignItems="center" padding={32}>
-      <img
-        src="./assets/Dynatrace_Logo.svg"
-        alt="Dynatrace Logo"
-        width={150}
-        height={150}
-        style={{ paddingBottom: 32 }}
-      ></img>
 
-      <Heading>Welcome To Your Dynatrace App</Heading>
-      <Paragraph>
-        Edit <Strong>src/app/pages/Home.tsx</Strong> and save to reload the app.
-      </Paragraph>
-      <Paragraph>
-        For more information and help on app development, check out the
-        following:
-      </Paragraph>
+      <Heading>APRA CPS 230 Compliance & Reporting</Heading>
 
       <Flex gap={48} paddingTop={64} flexFlow="wrap">
         <Card
-          href="/data"
-          inAppLink
+          href="https://guu84124.live.dynatrace.com/#dashboard;id=4302a03c-6c8c-4961-87b3-bb68c99922cf;applyDashboardDefaults=true"
           imgSrc={
-            theme === "light" ? "./assets/data.png" : "./assets/data_dark.png"
+            theme === "light" ? "./assets/monitor.png" : "./assets/monitor_dark.png"
           }
-          name="Explore data"
+          name="Monitor Platform"
         />
         <Card
-          href="https://dt-url.net/developers"
+          href="https://guu84124.apps.dynatrace.com/ui/document/v0/#share=8d45f603-932a-4587-9532-93667882fe57"
           imgSrc={
             theme === "light"
-              ? "./assets/devportal.png"
-              : "./assets/devportal_dark.png"
+              ? "./assets/analyse_n_evaluate.png"
+              : "./assets/analyse_n_evaluate_dark.png"
           }
-          name="Dynatrace Developer"
+          name="Analyse & Evaluate"
         />
         <Card
-          href="https://dt-url.net/devcommunity"
+          href="https://guu84124.live.dynatrace.com/#dashboard;id=60827a8a-ab6c-4ed6-93bd-a687d370b13b;gtf=-2h;gf=all"
           imgSrc={
             theme === "light"
-              ? "./assets/community.png"
-              : "./assets/community_dark.png"
+              ? "./assets/treat.png"
+              : "./assets/treat_dark.png"
           }
-          name="Developer Community"
+          name="Treat"
+        />
+        <Card
+          href="https://guu84124.apps.dynatrace.com/ui/document/v0/#share=f8f6716c-4aea-440e-ba17-7110414eaa46"
+          imgSrc={
+            theme === "light"
+              ? "./assets/record.png"
+              : "./assets/record_dark.png"
+          }
+          name="Record"
         />
       </Flex>
     </Flex>
